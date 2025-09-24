42635f7051699dfa3ccb325151a7063c02c90306
Lubhit Mamodia <lubhitmamodia@Lubhits-MacBook-Air.local>
2025-09-24 14:43:50 +0530

    Add library icon with transparent background and update LibraryPanel to use it

--- src/components/layout/LibraryPanel.tsx
+++ src/components/layout/LibraryPanel.tsx
@@ -48,7 +48,7 @@ const LibraryPanel: React.FC<LibraryPanelProps> = ({
                 <div className="relative w-7 h-7 flex items-center justify-center">
                   {/* Library Icon - Hidden on hover */}
                   <img 
-                    src="/dist/assets/Screenshot_2025-09-20_at_11.34.04_AM-removebg-preview.png"
+                    src="/library-icon.png"
                     alt="Library Icon"
                     className="w-7 h-7 group-hover:opacity-0 transition-opacity duration-200"
                   />
